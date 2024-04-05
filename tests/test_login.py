c12a7645e2b56c0220b1c658903b537d317b10e1
woori <whdnfl21@gmail.com>
2024-04-05 20:03:57 +0900

    rest api route test code

---
+++ tests/test_login.py
@@ -0,0 +1,22 @@
+import httpx
+import pytest
+
+@pytest.mark.asyncio
+async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
+    payload = {
+        "email" : "testuser@packt.com",
+        "password" : "testpassword"
+    }
+
+    headers = {
+        "accept" : "application/json",
+        "Content-Type" : "application/json"
+    }
+    test_response = {
+        "message" : "User created successfully."
+    }
+
+    response = await default_client.post("/user/signup", json=payload, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json() == test_response
